6da0db6dc51bc0ae740a2bf39b22666283e4b0b1
Roytjepele <56271222+Roytjepele@users.noreply.github.com>
2024-08-02 15:43:47 +0200

    Add files via upload

--- login.py
+++ login.py
@@ -1,24 +1,36 @@
 import sqlite3
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
-window = Tk()
-frm = ttk.Frame(window)
-frm.grid()
+window = tk.Tk()
+
 conn = sqlite3.connect('test.db')
 
 print("opened database successfully")
 
-# conn.execute("""CREATE TABLE COMPANY
-#              (ID INT PRIMARY KEY NOT NULL,
-#              NAME TEXT NOT NULL,
-#              AGE INT NOT NULL,
-#              ADRESS CHAR(50),
-#              SALARY REAL);""")
-id = int(input("Wat is het id-nummer?"))
-name = input("Wat is je naam?")
-age = int(input("wat is je leeftijd?"))
-adres = input("wat is je adres?")
+idlabel = tk.Label(text="Wat is het id-nummer?")
+id = tk.Entry(window)
+namelabel = tk.Label(text="Wat is je naam?")
+name = tk.Entry(window)
+agelabel = tk.Label(text="wat is je leeftijd?")
+age = tk.Entry(window)
+adreslabel = tk.Label(text="wat is je adres?")
+adres = tk.Entry(window)
+salarylabel = tk.Label(text="wat is je salaris?")
+salary = tk.Entry(window)
+entrybutton = tk.Button(text="save")
+
+idlabel.grid(row=0, column=0, padx=5, pady=5)
+id.grid(row=0, column=1, padx=5, pady=5)
+namelabel.grid(row=1, column=0, padx=5, pady=5)
+name.grid(row=1, column=1, padx=5, pady=5)
+agelabel.grid(row=2, column=0, padx=5, pady=5)
+age.grid(row=2, column=1, padx=5, pady=5)
+adreslabel.grid(row=3, column=0, padx=5, pady=5)
+adres.grid(row=3, column=1, padx=5, pady=5)
+salarylabel.grid(row=4, column=0, padx=5, pady=5)
+salary.grid(row=4, column=1, padx=5, pady=5)
+entrybutton.grid(row=5, column=1, padx=5, pady=5)
 
 window.mainloop()
 conn.close()
+print("closed database successfully")
