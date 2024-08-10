ea3451c93361c39f69d98aaf5ef1dfd8c716626c
Roytjepele <56271222+Roytjepele@users.noreply.github.com>
2024-08-10 13:37:30 +0200

    Add files via upload

--- login.py
+++ login.py
@@ -1,43 +1,77 @@
 import sqlite3
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk
+
+def enter_data():
+    firstname = first_name_entry.get()
+    lastname = last_name_entry.get()
+    title = title_combobox.get()
+    age = age_spinbox.get()
+    registered = reg_status_var.get()
+    # terms = terms_check.get()
+
+    print("first name: ", firstname, "last name:", lastname)
+    print("you are ", registered)
+
 
 window = tk.Tk()
+frame = tk.Frame(window)
+frame.pack()
 
-conn = sqlite3.connect('test.db')
+# conn = sqlite3.connect('test.db')
 
-print("opened database successfully")
+# print("opened database successfully")
 
-idlabel = tk.Label(text="What is the id-nummer?")
-id = tk.Entry(window)
-namelabel = tk.Label(text="What is your name?")
-name = tk.Entry(window)
-agelabel = tk.Label(text="What is your age?")
-age = tk.Entry(window)
-adresslabel = tk.Label(text="What is your adress?")
-adress = tk.Entry(window)
-salarylabel = tk.Label(text="What is your salary?")
-salary = tk.Entry(window)
+user_info_frame = tk.LabelFrame(frame, text="User information")
+user_info_frame.grid(column=0, row=0)
 
-def message():
-    messagebox.showinfo("title", f"id number is {id.get()}, name is {name.get()}, age is {age.get()}, adress is {adress.get()}, salary is {salary.get()}")
+first_name_label= tk.Label(user_info_frame, text="first name")
+first_name_label.grid(row=0, column=0)
+last_name_label = tk.Label(user_info_frame, text="last name")
+last_name_label.grid(row=1, column=0)
 
-entrybutton = tk.Button(text="save", command=message)
+first_name_entry = tk.Entry(user_info_frame)
+first_name_entry.grid(row=0, column=1)
+last_name_entry = tk.Entry(user_info_frame)
+last_name_entry.grid(row=1, column=1)
 
-idlabel.grid(row=0, column=0, padx=5, pady=5)
-id.grid(row=0, column=1, padx=5, pady=5)
-namelabel.grid(row=1, column=0, padx=5, pady=5)
-name.grid(row=1, column=1, padx=5, pady=5)
-agelabel.grid(row=2, column=0, padx=5, pady=5)
-age.grid(row=2, column=1, padx=5, pady=5)
-adresslabel.grid(row=3, column=0, padx=5, pady=5)
-adress.grid(row=3, column=1, padx=5, pady=5)
-salarylabel.grid(row=4, column=0, padx=5, pady=5)
-salary.grid(row=4, column=1, padx=5, pady=5)
-entrybutton.grid(row=5, column=1, padx=5, pady=5)
+title_label = tk.Label(user_info_frame, text="Title")
+title_combobox= ttk.Combobox(user_info_frame, values=["Mr.", "Ms.", "Dr."])
+title_label.grid(row=0, column=2)
+title_combobox.grid(row=0, column=3)
 
+age_label = tk.Label(user_info_frame, text="Age")
+age_spinbox = tk.Spinbox(user_info_frame, from_=18, to=110)
+age_label.grid(row=1, column=2)
+age_spinbox.grid(row=1, column=3)
 
+for widget in user_info_frame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+courses_form = tk.LabelFrame(frame)
+courses_form.grid(row=1, column=0, sticky="news")
+
+registered_label = tk.Label(courses_form, text="registration status")
+reg_status_var = tk.StringVar(value="not registered")
+registered_check = tk.Checkbutton(courses_form, text="currently registered",
+                                   variable=reg_status_var, onvalue="registered" , offvalue="not registered")
+registered_label.grid(row=0, column=0)
+registered_check.grid(row=0, column=1)
+
+for widget in courses_form.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+terms_frame = tk.LabelFrame(frame, text="terms & conditions")
+terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
+
+terms_check = tk.Checkbutton(terms_frame, text="I accept the terms and conditions.")
+terms_check.grid(row=0, column=0)
+
+button = tk.Button(frame, text="Enter data", command=enter_data)
+button.grid(row=3, column=0)
 
 window.mainloop()
-conn.close()
-print("closed database successfully")
+
+# conn.close()
+# print("closed database successfully")
+
