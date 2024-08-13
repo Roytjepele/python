4f52278214bd641f168d362a4b22de9f4fd543d6
Roytjepele <56271222+Roytjepele@users.noreply.github.com>
2024-08-13 21:55:15 +0200

    Add files via upload

--- data entry.py
+++ data entry.py
@@ -33,14 +33,16 @@ def enter_data():
 
 window = tk.Tk()
 frame = tk.Frame(window)
+frame.configure(bg='#333333')
 frame.pack()
 
-user_info_frame = tk.LabelFrame(frame, text="User information")
+user_info_frame = tk.LabelFrame(frame, text="User information",bg="#333333", fg="white")
+user_info_frame.configure(bg='#333333')
 user_info_frame.grid(column=0, row=0)
 
-first_name_label= tk.Label(user_info_frame, text="first name")
+first_name_label= tk.Label(user_info_frame, text="first name", bg="#333333", fg="white")
 first_name_label.grid(row=0, column=0)
-last_name_label = tk.Label(user_info_frame, text="last name")
+last_name_label = tk.Label(user_info_frame, text="last name", bg="#333333", fg="white")
 last_name_label.grid(row=1, column=0)
 
 first_name_entry = tk.Entry(user_info_frame)
@@ -48,12 +50,12 @@ first_name_entry.grid(row=0, column=1)
 last_name_entry = tk.Entry(user_info_frame)
 last_name_entry.grid(row=1, column=1)
 
-title_label = tk.Label(user_info_frame, text="Title")
+title_label = tk.Label(user_info_frame, text="Title", bg="#333333", fg="white")
 title_combobox= ttk.Combobox(user_info_frame, values=["Mr.", "Ms.", "Dr."])
 title_label.grid(row=0, column=2)
 title_combobox.grid(row=0, column=3)
 
-age_label = tk.Label(user_info_frame, text="Age")
+age_label = tk.Label(user_info_frame, text="Age", bg="#333333", fg="white")
 age_spinbox = tk.Spinbox(user_info_frame, from_=18, to=110)
 age_label.grid(row=1, column=2)
 age_spinbox.grid(row=1, column=3)
@@ -67,12 +69,14 @@ courses_form.grid(row=1, column=0, sticky="news")
 for widget in courses_form.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 
-terms_frame = tk.LabelFrame(frame, text="terms & conditions")
+terms_frame = tk.LabelFrame(frame, text="terms & conditions", fg="white")
+terms_frame.configure(bg='#333333')
 terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
 
 accept_var = tk.StringVar(value="not accepted")
 terms_check = tk.Checkbutton(terms_frame, text="I accept the terms and conditions.",
-                             variable=accept_var, onvalue="Accepted", offvalue="Not registered")
+                             variable=accept_var, onvalue="Accepted", offvalue="Not registered",
+                             bg="#333333", fg="white")
 terms_check.grid(row=0, column=0)
 
 button = tk.Button(frame, text="Enter data", command=enter_data)
--- login.py
+++ login.py
@@ -2,21 +2,21 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("login")
+window.geometry('340x440')
+window.configure(bg='#333333')
 
-frame = tk.Frame(window)
-frame.pack()
+login_label = tk.Label(window, text="Login", bg='#333333', fg='white')
+username_label = tk.Label(window, text="Username", bg='#333333', fg='white')
+username_entry = tk.Entry(window)
+password_entry = tk.Entry(window, show="*")
+password_label = tk.Label(window, text="Password", bg='#333333', fg='white')
+login_button = tk.Button(window, text="login")
 
-user_info_frame = tk.LabelFrame(frame, text="Login")
-user_info_frame.grid(column=0, row=0)
-
-login_label= tk.Label(user_info_frame, text="name")
-login_label.grid(row=0, column=0)
-password_label = tk.Label(user_info_frame, text="password")
-password_label.grid(row=1, column=0)
-
-login_entry = tk.Entry(user_info_frame)
-login_entry.grid(row=0, column=1)
-password_entry = tk.Entry(user_info_frame)
-password_entry.grid(row=1, column=1)
+login_label.grid(row=0, column=0, columnspan=2)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1, column=1)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1)
+login_button.grid(row=3, column=0, columnspan=2)
 
 window.mainloop()
