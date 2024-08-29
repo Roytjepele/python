35298fc651c308998ae11d847e0598f9924f5eb8
Roy van Zuijlen <56271222+Roytjepele@users.noreply.github.com>
2024-08-29 18:11:00 +0200

    Add files via upload

--- login_screen.py
+++ login_screen.py
@@ -1,41 +1,57 @@
 import tkinter as tk
+from tkinter import *
 import sqlite3
+from tkinter import messagebox as mb
+
+import oracledb
 
 window = tk.Tk()
-window.title("enter login")
-window.geometry('800x600')
-window.configure(bg='#333333')
+window.title("data entry form")
+# window.configure(bg='#333333')
 
-def login():
-    username = username_entry.get()
-    password = password_entry.get()
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    checkUsername = cursor.execute('SELECT username FROM user_login WHERE username=(?)', (username))
-    checkPassword = cursor.execute('SELECT username FROM user_login WHERE password=(?)', (password))
-    cursor.execute(checkUsername)
-    cursor.execute(checkPassword)
-    print(checkUsername)
-    conn.commit()
-    conn.close()
-    
+frame1 = tk.Frame(window)
+frame1.pack()
 
-frame = tk.Frame(bg='#333333')
+frame2 = tk.Frame(window)
 
-login_label = tk.Label(frame, text="Login", bg='#333333', fg='white', font=("Arial", 16))
-username_label = tk.Label(frame, text="Username", bg='#333333', fg='white', font=("Arial", 16))
-username_entry = tk.Entry(frame, font=("Arial", 16))
-password_entry = tk.Entry(frame, show="*", font=("Arial", 16))
-password_label = tk.Label(frame, text="Password", bg='#333333', fg='white', font=("Arial", 16))
-login_button = tk.Button(frame, text="login", command=login)
+conn = sqlite3.connect("data.db")
+c= conn.cursor()
+c.fetchall()
+conn.commit()
 
-login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
-username_label.grid(row=1, column=0)
-username_entry.grid(row=1, column=1, pady=20)
-password_label.grid(row=2, column=0)
-password_entry.grid(row=2, column=1, pady=20)
-login_button.grid(row=3, column=0, columnspan=2, pady=30)
+def check_user_data():
+        Username = username_entry.get()
+        password = password_entry.get()  
+        sql = "SELECT * FROM user_login WHERE username = '%s' AND password = '%s'" % (Username, password)
+        data = c.execute(sql)
+        if data.fetchone():
+                frame1.destroy()
+                frame2.pack()
+                username_label = Label(frame2, text=f"Username: {Username} is logged in.")
+                username_label.pack()
+                conn.close()
+        else:
+                mb.showerror(title="Error", message=f"Username: {Username} doesn't exist.")
+                
+        
+        
+#Saving User information in frame 1
+user_info_frame = tk.LabelFrame(frame1, text="User information")
+user_info_frame.grid(column=0, row=0)
 
-frame.pack()
+username_label= tk.Label(user_info_frame, text="username")
+username_label.grid(row=0, column=0)
+password_label = tk.Label(user_info_frame, text="password")
+password_label.grid(row=1, column=0)
+
+username_entry = Entry(user_info_frame)
+username_entry.grid(row=0, column=1)
+
+password_entry = tk.Entry(user_info_frame, show="*", font=("Arial", 16))
+password_entry.grid(row=1, column=1)
+
+Button1 = tk.Button(user_info_frame, text="click me!", command=check_user_data)
+Button1.grid(row=2, column=0)
+
 
 window.mainloop()
